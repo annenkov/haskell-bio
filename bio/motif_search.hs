c9f857702bebfee56f8b005cb3629ed7e5bd5c34
Danil Annenkov <support@ib-soft.ru>
2012-04-22 23:47:21 +0900

    Comments

--- bio/motif_search.hs
+++ bio/motif_search.hs
@@ -3,6 +3,8 @@ import Control.Monad
 import Control.Monad.Random
 import Test.HUnit
 
+-- Find motifs of length k in list of DNA sequences using "brute force" algorithm.
+-- Searching for minimum total distance between sequences and all 4^k k-mers
 bruteforceMedian seqs k = map snd $ filter f $ zip totalDistances words
                           where totalDistances = map (total_dH seqs) words
                                 words =  replicateM k dnaAlphabet
@@ -18,7 +20,10 @@ dH seq1 seq2 = sum $ zipWith dist seq1 seq2
 total_dH seqs k_mer = sum $ map min_dH seqs
                       where min_dH seq = minimum $ map (dH k_mer) $ k_mers (length k_mer) seq
 
--- all k-mers from sequence
+-- All k-mers from sequence
+-- For exapmle: 
+-- k_mers 3 "AACCTT" 
+-- ["AA","AC","CC","CT","TT"]
 k_mers k seq = map (take k) $ take (n-k+1) $ tails seq
               where n = length seq
 
