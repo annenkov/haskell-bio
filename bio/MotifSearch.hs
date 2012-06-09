dc43dae5c3f672073f4b0c2b34e6bc75576499b5
Danil Annenkov <support@ib-soft.ru>
2012-06-09 23:22:32 +0900

    Misc fixes

--- bio/MotifSearch.hs
+++ bio/MotifSearch.hs
@@ -1,3 +1,4 @@
+
 module MotifSearch where
 
 import Control.Monad
@@ -10,27 +11,27 @@ import Bio.Sequence.SeqData
 
 -- Find motifs of length k in list of DNA sequences using "brute force" algorithm.
 -- Searching for minimum total distance between sequences and all 4^k k-mers
-bruteforceMedianString seqs k = map snd $ filter f $ zip totalDistances words
+bruteforceMedianString seqs k = map snd $ filter minDist $ zip totalDistances words
                           where totalDistances = map (total_dH seqs) words
                                 words =  replicateM k dnaAlphabet
-                                f (n, _) = n == minimum totalDistances
-dnaAlphabet = "ACTG"
+                                minDist (n, _) = n == minimum totalDistances
+dnaAlphabet = "ACGT"
 
 -- Hamming distance
-dH seq1 seq2 = sum $ zipWith dist seq1 seq2
-                where dist a b | a==b      = 0
-                               | otherwise = 1
+dH seq1 seq2 = sum $ zipWith diff seq1 seq2
+               where diff a b | a == b    = 0
+                              | otherwise = 1
 
 -- Total Hamming Distance
-total_dH seqs k_mer = sum $ map min_dH seqs
-                      where min_dH seq = minimum $ map (dH k_mer) $ k_mers (length k_mer) seq
+total_dH seqs k_mer = sum $ map (min_dH k_mer) seqs
+                      where min_dH k_mer seq = minimum $ map (dH k_mer) $ k_mers (length k_mer) seq
 
 -- All k-mers from sequence
 -- For exapmle: 
 -- k_mers 3 "AACCTT" 
 -- ["AA","AC","CC","CT","TT"]
 k_mers k seq = map (take k) $ take (n-k+1) $ tails seq
-              where n = length seq
+               where n = length seq
 
 -- Branch-and-Bound median string search.
 
