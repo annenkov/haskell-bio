8877ed0c5b21fca5038a662794977da8eee91439
Danil Annenkov <support@ib-soft.ru>
2012-08-24 12:52:00 +0900

    Added test

--- bio/MotifSearch/MotifTests.hs
+++ bio/MotifSearch/MotifTests.hs
@@ -3,7 +3,7 @@ module MotifTests where
 import Control.Monad.Random
 import Test.HUnit
 import BruteforceMotifSearch (bruteforceMedianString, dnaAlphabet)
-import BBMotifSearch (bbMedianString)
+import BBMotifSearch (bbMedianString, preorderSearch)
 import System (getArgs)
 
 -- Random sequences for testing --
@@ -25,17 +25,21 @@ makeTestSeqs m n k_mer =  sequence $ replicate m $ implantMotif k_mer $ rndDNA n
 
 -- Tests --
 
-bruteforceMedianStringTest = TestCase (assertEqual "Finding 4-mer:" [k_mer] (bruteforceMedianString testData 4))
+bruteforceMedianStringTest = TestCase (assertEqual "Finding 4-mer:" [k_mer] (bruteforceMedianString testData $ length k_mer))
         where k_mer = "AAAA"
               testData = evalRand (makeTestSeqs 5 20 k_mer) (mkStdGen 1)
 
-brunchAndBoundMedianStringTest = TestCase (assertEqual "Finding 4-mer:" k_mer (bbMedianString testData 4))
+branchAndBoundMedianStringTest = TestCase (assertEqual "Finding 4-mer:" k_mer (bbMedianString testData $ length k_mer))
         where k_mer = "AAAA"
+              testData = evalRand (makeTestSeqs 5 20 k_mer) (mkStdGen 1)
+
+branchAndBoundMedianStringTestAlt = TestCase (assertEqual "Finding 4-mer:" k_mer (preorderSearch testData $ length k_mer))
+        where k_mer = "AACCT"
               testData = evalRand (makeTestSeqs 5 20 k_mer) (mkStdGen 1)
 
 
 -- run tests with "runTestTT tests"
-tests = TestList [bruteforceMedianStringTest, brunchAndBoundMedianStringTest]
+tests = TestList [bruteforceMedianStringTest, branchAndBoundMedianStringTest, branchAndBoundMedianStringTestAlt]
 
 main = do
     [n, m, k_mer] <- getArgs
